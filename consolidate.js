7c76ef107f12c47d0e3206738aea331e44243944
AMinehan <soloin@gmail.com>
2017-01-24 21:25:35 -0800

    adds readme and object.concat

--- consolidate.js
+++ consolidate.js
@@ -7,3 +7,15 @@ Object.defineProperty(Object.prototype, 'log', {
   enumerable: false,
   value: function(a){console.log(''+a+this); return this;}
 });
+
+Object.defineProperty(Object.prototype, 'concat', {
+  enumerable: false,
+  value: function(...args) {
+    args.forEach(arg=>{
+      for (var key in arg) {
+        this[key] = arg[key];
+      }
+    });
+  }
+  return this;
+});
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Consolidate
+
+This is a library of various functions for Array.prototype and Object.prototype.  They are designed to allow for more efficient and compact function chains and should therefore not be used by anyone, ever.
