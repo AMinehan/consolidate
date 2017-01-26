957568f2e5ddb39d3b1e873a9ae8e0d64c1c97de
AMinehan <soloin@gmail.com>
2017-01-25 22:36:11 -0800

    adds reduce

--- consolidate.js
+++ consolidate.js
@@ -1,11 +1,17 @@
+var log = function(arg){
+  arg = arg || '';
+  console.log(''+arg+this);
+  return this;
+}
+
 Object.defineProperty(Array.prototype, 'log', {
   enumerable: false,
-  value: function(a){console.log(''+a+this); return this;}
+  value: log
 });
 
 Object.defineProperty(Object.prototype, 'log', {
   enumerable: false,
-  value: function(a){console.log(''+a+this); return this;}
+  value: log
 });
 
 Object.defineProperty(Object.prototype, 'concat', {
@@ -19,3 +25,29 @@ Object.defineProperty(Object.prototype, 'concat', {
   }
   return this;
 });
+
+Object.defineProperty(Object.prototype, 'reduce', {
+  enumerable: false,
+  value: function(func, acc){
+    var keys = Object.keys(this);
+    var i;
+    if (arguments.length === 1){
+      i = 1;
+      var acc = this[keys[0]]
+    } else {
+      i = 0;
+    }
+    for (; i < keys.length; i++){
+      acc = func(acc, keys[i], this[keys[i]], i, this)
+    }
+    return acc;
+  }
+});
+
+
+
+//todo:
+//map, each, reduce, filter for Object
+//repeat for Object and Array
+//
+//
